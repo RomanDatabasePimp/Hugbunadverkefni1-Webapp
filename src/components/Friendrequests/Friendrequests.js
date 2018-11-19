29b0e0c23306a8fd9a37c3559260387b0cd1c451
Roman <roman.rumba@hotmail.com>
2018-11-19 18:01:45 +0000

    dd

--- src/components/Friendrequests/Friendrequests.js
+++ src/components/Friendrequests/Friendrequests.js
@@ -14,13 +14,30 @@ class Friendrequests extends Component {
   render() {
     /* array of user:{ user : {username:,displayName: }} */
     const {friendRequestors} = this.props;
-    
+    //{username: "dah38", displayName: "Davíð"}
+    const friendrequests = friendRequestors.length > 0 ? 
+    friendRequestors.map((item,key) => {
+     return(<li key={key}>
+              <p>{item.displayName}</p>
+            </li>)
+    }) : <li>Looks like you have no friend requests</li>;
 
     
 
     return (
-      <div>
-        
+      <div className="border">
+        <h3>Friend Requests</h3>
+        <ul className="horizontal_list">
+          {friendrequests}
+          <div>
+  <input type="checkbox" id="horns" name="horns" />
+  <label for="horns">Accept</label>
+</div>
+<div>
+  <input type="checkbox" id="horns" name="horns" />
+  <label for="horns">Reject</label>
+</div>
+        </ul>
       </div>
     );
   }
@@ -31,9 +48,10 @@ class Friendrequests extends Component {
   auth er reducer sem er sameinaður i index.js */
 const mapStateToProps = (state) => {
   return {
+  
   }
 }
-  
+
 /* make this component aware of the aplication store 
    þetta er ekki lengur component heldur Container */
  export default connect(mapStateToProps)(Friendrequests);
